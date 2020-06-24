06d9fa4b0aed39d1d94e21a96386de90d564598d
AdrianSA <Adrian.angara@gmail.com>
2020-06-24 14:38:06 -0400

    Added some initializations for each function to test compilation

--- code/Assignment1.java
+++ code/Assignment1.java
@@ -4,33 +4,43 @@ public class Assignment1{
   
   public int[][] denseMatrixMult(int[][] A, int[][] B, int size)
   {
+    int[][] arr2 = new int[0][];
 
+    return arr2;
   }
   
   public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
   {
- 
+    int[][] arr2 = new int[0][];
+
+    return arr2;
   }
   
   public int[][] sub(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
   {
+    int[][] arr2 = new int[0][];
 
+    return arr2;
   }
   
   
   public int[][] initMatrix(int n)
   {
+    int[][] arr2 = new int[0][];
 
+    return arr2;
   }
   
   public void printMatrix(int n, int[][] A)
   {
- 
+
   }
   
   public int[][] readMatrix(String filename, int n) throws Exception
   {
- 
+    int[][] arr2 = new int[0][];
+
+    return arr2;
   }
   
 }
